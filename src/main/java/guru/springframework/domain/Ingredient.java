a84950186217f775a65e9cca9f0e671b05c8b278
Jonathan Moraes <jonathan.pmoraes@senacsp.edu.br>
2023-03-11 21:26:43 -0300

    updating project

---
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -0,0 +1,38 @@
+package guru.springframework.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+    @ManyToOne
+    private Recipe recipe;
+
+    public Ingredient() {
+    }
+
+    public Ingredient(String description, BigDecimal amount) {
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public Ingredient(String description, BigDecimal amount, Recipe recipe) {
+        this.description = description;
+        this.amount = amount;
+        this.recipe = recipe;
+    }
+
+}
